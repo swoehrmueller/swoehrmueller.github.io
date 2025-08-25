2606f3383ac90653f149427a96d7b3e01fcf9cb2
swoehrmueller <s.woehrmueller@googlemail.com>
2025-08-26 00:17:55 +0200

    add blog as a test without links

---
+++ assets/js/blogData.js
@@ -0,0 +1,22 @@
+// assets/js/blogData.js
+const blogPosts = [
+    {
+        title: "My First Blog Post",
+        description: "This blog post is a test.",
+        image: "assets/img/bg.jpg",
+        link: "assets/files/blogposts/post1.html"
+    }
+    // {
+    //     title: "Insights from Economic Modeling",
+    //     description: "Breaking down some complex macroeconomic concepts into something accessible.",
+    //     image: "assets/img/blog2.jpg",
+    //     link: "assets/files/blogposts/post2.html"
+    // },
+    // {
+    //     title: "Data Visualization Tips",
+    //     description: "A quick guide on how I create charts and visuals for my research papers.",
+    //     image: "assets/img/blog3.jpg",
+    //     link: "assets/files/blogposts/post3.html"
+    // }
+];
+
